31064f9046808262ef2a574a2362af6f7ed3bedb
Juraj Paluba <juraj.palubajp@gmail.com>
2025-02-13 23:29:18 +0100

    feat(docker-api): initialize docker api client
    For some reason, latest tokio version does not work for me, it cannot
    compile due to missing versions.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use bollard::Docker;
+
+#[tokio::main]
+async fn main() {
+    let docker_client = Docker::connect_with_socket_defaults().unwrap();
+    let ping_res = docker_client.ping().await.unwrap();
+    println!("{}", ping_res);
 }
