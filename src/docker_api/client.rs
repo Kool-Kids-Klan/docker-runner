caf8fdfef4a2d0fe7a6cf46f7e4f2b1d1138a02f
Juraj Paluba <juraj.palubajp@gmail.com>
2025-02-14 23:42:56 +0100

    feat(docker_api): create a pull_image function
    The error handling is not good. Will try to fix it.

--- src/docker_api/client.rs
+++ src/docker_api/client.rs
@@ -1,4 +1,10 @@
-use bollard::{errors::Error, image::ListImagesOptions, secret::ImageSummary, Docker};
+use bollard::{
+    errors::Error,
+    image::{CreateImageOptions, ListImagesOptions},
+    secret::{CreateImageInfo, ImageSummary},
+    Docker,
+};
+use futures_util::TryStreamExt;
 
 pub fn get_docker_client() -> Result<Docker, Error> {
     return Docker::connect_with_socket_defaults();
@@ -10,5 +16,24 @@ pub async fn list_images(client: &Docker) -> Result<Vec<ImageSummary>, Error> {
             all: true,
             ..Default::default()
         }))
+        .await;
+}
+
+pub async fn pull_image(
+    client: &Docker,
+    name: &str,
+    tag: &str,
+) -> Result<Vec<CreateImageInfo>, Error> {
+    return client
+        .create_image(
+            Some(CreateImageOptions {
+                from_image: name,
+                tag,
+                ..Default::default()
+            }),
+            None,
+            None,
+        )
+        .try_collect()
         .await;
 }
