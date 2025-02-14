346ed052237776f614e82ae750cd72e6af85c067
Juraj Paluba <juraj.palubajp@gmail.com>
2025-02-14 22:50:02 +0100

    feat(docker-api): implement function to list images

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use bollard::Docker;
+mod docker_api;
+use docker_api::client::{get_docker_client, list_images};
 
 #[tokio::main]
 async fn main() {
-    let docker_client = Docker::connect_with_socket_defaults().unwrap();
+    let docker_client = get_docker_client().unwrap();
     let ping_res = docker_client.ping().await.unwrap();
     println!("{}", ping_res);
+    let images = list_images(&docker_client).await.unwrap();
+    for image in images {
+        println!("-> {:?}", image);
+    }
 }
