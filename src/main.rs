6a45bd2a4f50a6c826ec7e67cc58fe28426fbf04
Juraj Paluba <juraj.palubajp@gmail.com>
2025-02-15 18:25:46 +0100

    feat(docker_api): create run_container method

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod docker_api;
-use docker_api::client::{get_docker_client, list_images, pull_image};
+use docker_api::client::{get_docker_client, list_images, pull_image, run_container};
 
 #[tokio::main]
 async fn main() {
@@ -11,8 +11,17 @@ async fn main() {
         println!("-> {:?}", image);
     }
 
-    let pull_logs = pull_image(&docker_client, "nginx", "latest").await.unwrap();
+    let image_name = "ubuntu";
+    let pull_logs = pull_image(&docker_client, image_name, "latest")
+        .await
+        .unwrap();
     for log in pull_logs {
         println!("-> {:?}", log);
     }
+
+    let container_name = "my-contianer";
+    let created_container = run_container(&docker_client, container_name, image_name)
+        .await
+        .unwrap();
+    println!("-> {:?}", created_container);
 }
